fcaa0b801f40c22c050431c06e2826b98eefd1bd
AlinurAbdyiskakov <75415137+AlinurAbdyiskakov@users.noreply.github.com>
2021-08-15 20:40:36 +0600

    add new vidio

--- src/components/Vidio/Vidio.js
+++ src/components/Vidio/Vidio.js
@@ -2,7 +2,7 @@ import React from 'react'
 import classes from '../Vidio/Vidio.module.css'
 import vidio from '../../vidio/vidio.mp4'
 import vidio2 from '../../vidio/vidio2.mp4'
-
+import vidio3 from '../../vidio/vidio3.mp4'
 // import mp4 from'./https://youtu.be/gnNvrq977B8'
 function Vidio() {
     return ( <
@@ -16,7 +16,7 @@ function Vidio() {
         controls loop > < /video> <
         video src = { vidio2 }
         controls loop > < /video>  <
-        video src = { vidio }
+        video src = { vidio3 }
         controls loop > < /video>  <
         video src = { vidio }
         controls loop > < /video> <
