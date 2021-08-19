be80ded4db59e825c82220e72cead36affdfc5ac
AlinurAbdyiskakov <75415137+AlinurAbdyiskakov@users.noreply.github.com>
2021-08-19 20:56:02 +0600

    фвв мшвшщ

--- src/components/Anime/Anime.js
+++ src/components/Anime/Anime.js
@@ -3,22 +3,37 @@ import './Anime.scss'
 import anime_1 from '../../vidio/anime_2.mp4';
 import anime_2 from '../../vidio/anime_1.mp4';
 import anime_3 from '../../vidio/anime_3.mp4';
-function  Anime  (){
-    return (
-        <div class="Anime">
-            <div class="anime_1">
- <video src={anime_1} controls loop></video>
- <h1>аниме гаргантия на зелёной планете</h1>
- </div>
- <div class="anime_1">
- <video src={anime_2} controls loop></video>
- <h1>аниме невиданный цветок</h1>
- </div>
- <div class="anime_1">
- <video src={anime_3} controls loop></video>
- <h1> повседневная жизнь бессмертного короля</h1>
- </div>
-        </div>
+import anime_4 from '../../vidio/anime_4.mp4';
+
+function Anime() {
+    return ( <
+        div class = "Anime" >
+        <
+        div class = "anime_1" >
+        <
+        video src = { anime_1 }
+        controls loop > < /video> <
+        h1 > аниме гаргантия на зелёной планете < /h1> <
+        /div> <
+        div class = "anime_1" >
+        <
+        video src = { anime_2 }
+        controls loop > < /video> <
+        h1 > аниме невиданный цветок < /h1> <
+        /div> <
+        div class = "anime_1" >
+        <
+        video src = { anime_3 }
+        controls loop > < /video> <
+        h1 > повседневная жизнь бессмертного короля < /h1> <
+        /div> <
+        div class = "anime_1" >
+        <
+        video src = { anime_4 }
+        controls loop > < /video> <
+        h1 > Аниме Насколько тяжёлые гантели ты сможешь поднять < /h1> <
+        /div> <
+        /div>
     )
 }
 
