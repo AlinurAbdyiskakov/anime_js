eab9d701da49e8a86682bfbbba547a38ebcb1d75
AlinurAbdyiskakov <75415137+AlinurAbdyiskakov@users.noreply.github.com>
2021-08-19 20:15:19 +0600

    add new vidio in the Vidio.js

--- src/components/Vidio/Vidio.js
+++ src/components/Vidio/Vidio.js
@@ -3,6 +3,8 @@ import classes from '../Vidio/Vidio.module.css'
 import vidio from '../../vidio/vidio.mp4'
 import vidio2 from '../../vidio/vidio2.mp4'
 import vidio3 from '../../vidio/vidio3.mp4'
+import vidio4 from '../../vidio/vidio4.mp4'
+import vidio5 from '../../vidio/vidio5.mp4'
 // import mp4 from'./https://youtu.be/gnNvrq977B8'
 function Vidio() {
     return ( <
@@ -16,9 +18,11 @@ function Vidio() {
         video src = { vidio2 }
         controls loop > < /video>  <
         video src = { vidio3 }
-        controls loop > < /video> 
-
-        <
+        controls loop > < /video>  <
+        video src = { vidio4 }
+        controls loop > < /video>  <
+        video src = { vidio5 }
+        controls loop > < /video>  <
         /div>
     )
 }
