3e3ba8cdabce9e3e2271a6ec6e20ed97587df226
AlinurAbdyiskakov <75415137+AlinurAbdyiskakov@users.noreply.github.com>
2021-08-19 20:28:00 +0600

    stilization abd add img

--- src/components/Redux_1/Redux_1.js
+++ src/components/Redux_1/Redux_1.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import classes from './Redux_1.module.css'
 import in1 from '../../imges/in1.jpg'
+import in2 from '../../imges/in2.jpg'
+import in3 from '../../imges/in3.jpg'
+import in4 from '../../imges/in4.jpg'
 
 function Redux_1() {
     return ( <
@@ -11,12 +14,38 @@ function Redux_1() {
         div className = { classes.in1 } >
         <
         img src = { in1 }
-        alt = "interesting" / >
+        alt = "interesting-1" / >
         <
         p > < a href = "http://ai.mof.x10.bz/promises/" > ai.mof.x10.bz / promises < /a>-  сайт, на котором можно отправлять и просматривать анонимные обещания его посетителей</p >
         <
-        /div> </div >
+        /div>  <
+        div className = { classes.in1 } >
         <
+        img src = { in2 }
+        alt = "interesting-2" / >
+        <
+        p > < a href = "https://lastminute-auction.com/" > lastminute - auction.com < /a>— проект, позволяющий просматривать дешевые аукционы с Ebay в последние секунды перед их закрытием</p >
+        <
+        /div>  <
+        div className = { classes.in1 } >
+        <
+        img src = { in4 }
+        alt = "interesting-3" / >
+        <
+        p > < a href = "https://documentaryheaven.com/" > https: //documentaryheaven.com/ </a>--тут собраны лучшие документальные фильмы, которые можно посмотреть бесплатно</p>
+        <
+        /div>  <
+        div className = { classes.in1 } >
+        <
+        img src = { in3 }
+        alt = "interesting-3" / >
+        <
+        p > < a href = "  https://gifparty.dance/" > https: //gifparty.dance/</a>The best party on the internet!о</p>
+        <
+        /div> 
+
+        <
+        /div> <
         /div>
     )
 }
