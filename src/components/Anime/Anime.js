c1e8ead2e3a6acaa80578863c94afa614369aab3
AlinurAbdyiskakov <75415137+AlinurAbdyiskakov@users.noreply.github.com>
2021-08-23 21:10:04 +0600

    add vidio anime

--- src/components/Anime/Anime.js
+++ src/components/Anime/Anime.js
@@ -4,6 +4,9 @@ import anime_1 from '../../vidio/anime_2.mp4';
 import anime_2 from '../../vidio/anime_1.mp4';
 import anime_3 from '../../vidio/anime_3.mp4';
 import anime_4 from '../../vidio/anime_4.mp4';
+import anime_5 from '../../vidio/anime_5.mp4';
+import anime_6 from '../../vidio/anime_6.mp4';
+import anime_7 from '../../vidio/anime7.mp4';
 
 function Anime() {
     return ( < div class = "Anime" >
@@ -36,6 +39,24 @@ function Anime() {
         video src = { anime_4 }
         controls loop > < /video> <
         h1 > Аниме Насколько тяжёлые гантели ты сможешь поднять < /h1>  <
+        /div> <
+        div class = "anime_1" >
+        <
+        video src = { anime_5 }
+        controls loop > < /video> <
+        h1 > Аниме Сага о Винланде < /h1>  <
+        /div> <
+        div class = "anime_1" >
+        <
+        video src = { anime_6 }
+        controls loop > < /video> <
+        h1 > Аниме Мфг на полную ставку < /h1>  <
+        /div> <
+        div class = "anime_1" >
+        <
+        video src = { anime_7 }
+        controls loop > < /video> <
+        h1 > Аниме Восхождения героя щита < /h1>  <
         /div> <
         /div>
     )
