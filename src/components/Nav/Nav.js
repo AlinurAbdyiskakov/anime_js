030aa47a9d91eeedf8c1b5b930984b8ccafd5429
AlinurAbdyiskakov <75415137+AlinurAbdyiskakov@users.noreply.github.com>
2021-08-24 20:10:12 +0600

    delect in the Nav.js Link

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,11 @@
 import classes from "./Nav.module.css";
 import NavItem from "../NavItem/NavItem";
-import { Link } from "react-router-dom";
+
 
 const Nav = () => {
     return ( < ul className = { classes.Nav } >
+
         <
-        select > < /select> <
         NavItem url = "/"
         exact > Home < /NavItem>  <
         NavItem url = "/musec" > Musec < /NavItem>
