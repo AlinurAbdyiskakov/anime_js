d1d642b547c9381de678b8ee50c6e98102ee23d2
AlinurAbdyiskakov <75415137+AlinurAbdyiskakov@users.noreply.github.com>
2021-09-08 19:57:22 +0600

    the change in the Home.js add  forms

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,8 +3,25 @@ import React from 'react'
 function Home() {
     return ( <
         div >
-
         <
+        h1 > The problems < /h1> <
+        form >
+        <
+        label > name < /label> <
+        input type = "text"
+        name = "name" / >
+        <
+        label > Email < /label> <
+        input type = "email"
+        name = "email" / >
+        <
+        label > massage < /label> <
+        textarea name = "massage"
+        rows = "5" / >
+        <
+        input type = "submit" / >
+        <
+        /form> <
         /div>
     )
 }
