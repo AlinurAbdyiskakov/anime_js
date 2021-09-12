3005749af1c6e3cb9abb6fb1f5e3fab6b64b9ba7
AlinurAbdyiskakov <75415137+AlinurAbdyiskakov@users.noreply.github.com>
2021-09-12 19:50:43 +0600

    add moon and sun svg icons

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,7 @@
 import classes from "./Nav.module.css";
 import NavItem from "../NavItem/NavItem";
-
-
+import moon from "../../imges/moon.svg"
+import sun from "../../imges/sun.svg"
 const Nav = () => {
     return ( < ul className = { classes.Nav } >
 
@@ -12,9 +12,16 @@ const Nav = () => {
 
         <
         NavItem url = "/anime" > Anime < /NavItem>  <
-        NavItem url = "/redux" > Interesting sites < /NavItem>  
-
+        NavItem url = "/redux" > Interesting sites < /NavItem>  <
+        div className = { classes.con1 } >
         <
+        img className = { classes.img1 }
+        src = { moon }
+        alt = "moon svg" / >
+        <
+        img className = { classes.img1 }
+        src = { sun }
+        alt = "sun svg" / > < /div>  <
         /ul>
     )
 }
