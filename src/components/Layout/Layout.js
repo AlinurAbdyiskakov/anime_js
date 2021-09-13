e7940a856e8e1d060f733cc47cbfd1b704f6821b
AlinurAbdyiskakov <75415137+AlinurAbdyiskakov@users.noreply.github.com>
2021-09-13 21:07:03 +0600

    delect Input.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,14 +6,9 @@ import classes from "./Layout.module.css";
 const Layout = ({ children }) => {
     const [drawerOpen, setDrawerOpen] = useState(false);
 
-    return ( < div className = { classes.Layout } >
-        <
-        Toolbar openDrawer = {
-            () => setDrawerOpen(true) }
-        /> <Drawer open = { drawerOpen }
-        closeDrawer = {
-            () => setDrawerOpen(false) }
-        />  <main > { children } </main > < /div>
+    return ( <div className = { classes.Layout } >
+        <Toolbar openDrawer = {() => setDrawerOpen(true) }/> <Drawer open = { drawerOpen }
+        closeDrawer = {() => setDrawerOpen(false) }/>  <main > { children } </main> </div>
     );
 }
 
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,13 +3,11 @@ import logo from '../../imges/logo.png';
 import classes from './Logo.module.css'
 
 function Logo() {
-    return ( < div className = { classes.Logo } >
-        <
-        img src = { logo }
-        alt = "logo" / >
-
-        <
-        /div>
+    return ( <div className = { classes.Logo } >
+        <img src = { logo }
+        alt = "logo" />
+   
+        </div>
     )
 }
 
