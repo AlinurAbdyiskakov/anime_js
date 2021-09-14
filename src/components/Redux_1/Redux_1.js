9684a2b06d066b9bde3cc3802470dadeec9be727
AlinurAbdyiskakov <75415137+AlinurAbdyiskakov@users.noreply.github.com>
2021-09-14 22:14:12 +0600

    add new img k interesting list

--- src/components/Redux_1/Redux_1.js
+++ src/components/Redux_1/Redux_1.js
@@ -1,34 +1,62 @@
-
 import React from 'react'
 import classes from './Redux_1.module.css'
 import in1 from '../../imges/in1.jpg'
 import in2 from '../../imges/in2.jpg'
 import in3 from '../../imges/in3.jpg'
 import in4 from '../../imges/in4.jpg'
+import in5 from '../../imges/in5.jpg'
+
 function Redux_1() {
-  return (
-    <div>
-      <div className={classes.Redux_1}>
-        <div className={classes.in1} >
-     <img  src={in1} alt="interesting-1"/>
-   <p><a href="http://ai.mof.x10.bz/promises/">ai.mof.x10.bz/promises</a>-  сайт, на котором можно отправлять и просматривать анонимные обещания его посетителей</p>
-     </div> 
-     <div className={classes.in1} >
-     <img  src={in2} alt="interesting-2"/>
-   <p><a href= "https://lastminute-auction.com/">lastminute-auction.com </a>— проект, позволяющий просматривать дешевые аукционы с Ebay в последние секунды перед их закрытием</p>
-     </div> 
-     <div className={classes.in1} >
-     <img  src={in4} alt="interesting-3"/>
-   <p><a href= "https://documentaryheaven.com/">https://documentaryheaven.com/ </a>--тут собраны лучшие документальные фильмы, которые можно посмотреть бесплатно</p>
-     </div> 
-     <div className={classes.in1} >
-     <img  src={in3} alt="interesting-3"/>
-   <p><a href= "  https://gifparty.dance/">  https://gifparty.dance/</a>The best party on the internet!о</p>
-     </div> 
-   
-    </div>
-    </div>
-  )
+    return ( <
+        div >
+        <
+        div className = { classes.Redux_1 } >
+        <
+        div className = { classes.in1 } >
+        <
+        img src = { in1 }
+        alt = "interesting-1" / >
+        <
+        p > < a href = "http://ai.mof.x10.bz/promises/" > ai.mof.x10.bz / promises < /a>-  сайт, на котором можно отправлять и просматривать анонимные обещания его посетителей</p >
+        <
+        /div>  <
+        div className = { classes.in1 } >
+        <
+        img src = { in2 }
+        alt = "interesting-2" / >
+        <
+        p > < a href = "https://lastminute-auction.com/" > lastminute - auction.com < /a>— проект, позволяющий просматривать дешевые аукционы с Ebay в последние секунды перед их закрытием</p >
+        <
+        /div>  <
+        div className = { classes.in1 } >
+        <
+        img src = { in4 }
+        alt = "interesting-3" / >
+        <
+        p > < a href = "https://documentaryheaven.com/" > https: //documentaryheaven.com/ </a>--тут собраны лучшие документальные фильмы, которые можно посмотреть бесплатно</p>
+        <
+        /div>  <
+        div className = { classes.in1 } >
+        <
+        img src = { in3 }
+        alt = "interesting-3" / >
+        <
+        p > < a href = "  https://gifparty.dance/" > https: //gifparty.dance/</a>The best party on the internet!о</p>
+        <
+        /div>  <
+        div className = { classes.in1 } >
+        <
+        img src = { in5 }
+        alt = "interesting-3" / >
+        <
+        p > < a href = "   https://t.me/joinchat/w30-xQNIno0wOWRi" > t.me / denroom < /a> авторский канал, на котором публикуют информацию из Теневого интернета и расскажут о том, о чем говорить не принято и довольно опасно!</p >
+        <
+        /div> 
+
+        <
+        /div> <
+        /div>
+    )
 }
 
 export default Redux_1
